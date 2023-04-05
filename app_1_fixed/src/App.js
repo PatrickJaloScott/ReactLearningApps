597b8a0cd12d3ef867994b04b5e2513d8830d535
PatrickJaloScott <patrickjalo.scott@gmail.com>
2023-04-05 14:28:47 +0300

    Restored app_1 as app_1_fixed Changed index.html titles to React App [x]

---
+++ app_1_fixed/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+export default function Bio() {
+  return (
+      <>
+        <div className="intro">
+          <h1>Welcome to my website!</h1>
+        </div>
+        <p className="summary">
+          You can find my thoughts here.
+          <br/><br/>
+          And <b><i>pictures</i></b> of scientists!
+        </p>
+      </>
+  );
+}
